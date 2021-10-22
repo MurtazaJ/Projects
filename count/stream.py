158a9fbcf68c5a49339697950bf7312787a5aac3
Murtaza <murtazajits@gmail.com>
2021-10-22 02:57:21 +0200

    Update stream.py

--- count/stream.py
+++ count/stream.py
@@ -1,10 +1,82 @@
 import streamlit as st
 import cv2
-import PoseModule as pm
 import numpy as np
 import time
 
-detector = pm.poseDetector()
+
+class poseDetector():
+ 
+    def __init__(self, mode=False, upBody=False, smooth=True,
+                 detectionCon=0.5, trackCon=0.5):
+ 
+        self.mode = mode
+        self.upBody = upBody
+        self.smooth = smooth
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+ 
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(self.mode, self.upBody, self.smooth,
+                                     self.detectionCon, self.trackCon)
+ 
+    def findPose(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.pose.process(imgRGB)
+        if self.results.pose_landmarks:
+            if draw:
+                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks,
+                                           self.mpPose.POSE_CONNECTIONS)
+        return img
+ 
+    def findPosition(self, img, draw=True):
+        self.lmList = []
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h, w, c = img.shape
+                # print(id, lm)
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                self.lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+        return self.lmList
+ 
+    def findAngle(self, img, p1, p2, p3, draw=True):
+ 
+        # Get the landmarks
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        x3, y3 = self.lmList[p3][1:]
+ 
+        # Calculate the Angle
+        # angle = math.degrees(math.atan2(y3 - y2, x3 - x2) -
+        #                      math.atan2(y1 - y2, x1 - x2))
+        # if angle < 0:
+        #     angle += 360
+        radians = np.arctan2(y3 - y2, x3 - x2) - np.arctan2(y1 - y2, x1 - x2)
+        angle   = np.abs(radians*180/np.pi) 
+
+        if angle >180:
+            angle = 360 - angle
+        # print(angle)
+ 
+        # Draw
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 255, 255), 3)
+            cv2.line(img, (x3, y3), (x2, y2), (255, 255, 255), 3)
+            cv2.circle(img, (x1, y1), 5, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x1, y1), 10, (0, 0, 255), 2)
+            cv2.circle(img, (x2, y2), 5, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 10, (0, 0, 255), 2)
+            cv2.circle(img, (x3, y3), 5, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x3, y3), 10, (0, 0, 255), 2)
+            # cv2.putText(img, str(int(angle)), (x2 - 80, y2 + 50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+            cv2.putText(img, str(int(angle)), tuple(np.add(self.lmList[p2][1:],[20,20]).astype(int) ),
+                        cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2)
+                        
+        return angle
+
+detector = poseDetector()
 count = 0
 dir   = 0
 pTime = 0
