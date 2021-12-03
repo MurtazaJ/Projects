04070eed194d83aa45d8cc581a60a25fc07d30e9
Murtaza <murtazajits@gmail.com>
2021-12-03 12:16:29 +0100

    init

--- MRPCal/calculator.py
+++ MRPCal/calculator.py
@@ -10,9 +10,19 @@ import streamlit as st
 st.set_page_config(layout = 'wide')
 st.header('MRP Calculator')
 
-# Defining the ladies and Gents functions
+
+#Defining rounding functions
+def round_to_nearest(n):
+    if n ==0:
+        return 0
+    else:
+        r = n % 10
+    
+        return (n + 10 - r if r + r >= 10 else n - r)-1
+
+# Defining the ladies and Gents functions    
 def mrp_calculator_gents():
-    GST        = st.sidebar.selectbox('', (5, 18))
+    GST        = st.sidebar.selectbox('', (12, 18))
     cp         = st.number_input('Please Enter the Cost Price', 0)
 
     gst_amt    = cp* (GST/100)
@@ -21,34 +31,34 @@ def mrp_calculator_gents():
 
     total      = cp + gst_amt + expenses
 
-    if cp >= 600:
-        if GST == 18:
-            mrp     = total + cp + (total*.1) - expenses
-        else:
-            mrp = (total ) + cp - expenses - (2*gst_amt)
+    # if cp >= 600:
+    #     if GST == 18:
+    #         mrp     = total + cp + (total*.1) - expenses
+    #     else:
+#         mrp = (total ) + cp - expenses - (2*gst_amt)
+    if GST == 18:
+        mrp = int(total + cp + (total*.1) + gst_amt)
+        mrp = round_to_nearest(mrp)
     else:
-        if GST == 18:
-            mrp     = total + cp + (total*.1) - expenses
-        else:
-            mrp = (total ) + cp 
-    
+        mrp = int((total ) + cp + gst_amt)
+        mrp = round_to_nearest(mrp)
     return mrp , gst_amt, expenses
        
 def mrp_calculator_ladies():
-    GST        = st.sidebar.selectbox('', (5, 18))
-    cp         = st.number_input('Please Enter the Cost Price', 0)
+    GST = st.sidebar.selectbox('', (12, 18))
+    cp  = st.number_input('Please Enter the Cost Price', 0)
 
-    gst_amt    = cp* (GST/100)
+    gst_amt = cp* (GST/100)
 
-    expenses   = (cp + gst_amt) *(2/100)
+    expenses = (cp + gst_amt) *(2/100)
 
-    total      = cp + gst_amt + expenses
-
+    total = cp + gst_amt + expenses
     if GST == 18:
-        mrp     = total + cp  - expenses 
+        mrp = int(total + cp  + gst_amt) 
+        mrp = round_to_nearest(mrp)
     else:
-        mrp = (total) + cp - expenses 
-    
+        mrp = int((total) + cp  + gst_amt)
+        mrp = round_to_nearest(mrp)
     return mrp , gst_amt, expenses
 
 # Running the Model
