ccd1fb3d293c37ff7caab2cec54ef2c9278a695d
Murtaza <murtazajits@gmail.com>
2022-01-19 23:56:11 +0100

    new project

---
+++ amazon_recommendation/new_business.py
@@ -0,0 +1,44 @@
+#%%
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.neighbors import NearestNeighbors
+from sklearn.cluster import KMeans
+from sklearn.metrics import adjusted_rand_score
+import pandas as pd
+
+#%%
+product_descriptions = pd.read_csv('data/product_descriptions.csv.zip')
+product_descriptions.shape
+# %%
+product_descriptions = product_descriptions.dropna()
+product_descriptions.shape
+product_descriptions.head()
+# %%
+product_descriptions1 = product_descriptions.head(500)
+# product_descriptions1.iloc[:,1]
+
+product_descriptions1["product_description"].head(10)
+# %%
+vectorizer = TfidfVectorizer(stop_words='english')
+X1 = vectorizer.fit_transform(product_descriptions1["product_description"])
+X1
+# %%
+
+kmeans = KMeans(n_clusters = 10, init = 'k-means++')
+y_kmeans = kmeans.fit_predict(X1)
+# %%
+def print_cluster(i):
+    print("Cluster %d:" % i),
+    for ind in order_centroids[i, :10]:
+        print(' %s' % terms[ind]),
+    print
+# %%
+true_k = 50
+
+model = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1)
+model.fit(X1)
+
+print("Top terms per cluster:")
+order_centroids = model.cluster_centers_.argsort()[:, ::-1]
+terms = vectorizer.get_feature_names()
+for i in range(true_k):
+    print_cluster(i)
