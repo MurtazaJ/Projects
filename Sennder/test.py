8556bfc81e6d706ea98cad93a94f750029bd6152
Murtaza <murtazajits@gmail.com>
2022-07-19 15:01:05 +0200

    Create test.py

---
+++ Sennder/test.py
@@ -0,0 +1,27 @@
+# import re
+
+# x = 'is2 Thi1s T4est 3a' 
+# def remove_special_characters(x):
+#     m=[]
+#     for i in (re.split(r'(\d+)', x)):
+        
+#         if i.isdigit():
+#             int(i)
+#         m.append(i)
+            
+#         return m
+        
+# # print(x.sort(key=remove_special_characters))
+# print(remove_special_characters(x))
+# print(x.isdigit())
+# def array_diff(a, b):
+#     for i in b:
+#         if i in a:
+#             a = list(filter(lambda j: j!=i, a ))
+#     return a
+    
+# a = [1,2]
+# b = [1]
+# print(array_diff(a, b))
+import math
+print(int(math.sqrt(26)+0.5) **2)
