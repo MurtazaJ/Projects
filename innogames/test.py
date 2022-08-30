7d08fb6d04667796c5a44afa40d239ab2f349cdb
Murtaza <murtazajits@gmail.com>
2022-08-30 04:57:55 +0200

    new updates

--- innogames/test.py
+++ innogames/test.py
@@ -1,14 +1,14 @@
-import pickle
-from model import model_test
-import pandas as pd
-import numpy as np
+# import pickle
+# from model import model_test
+# import pandas as pd
+# import numpy as np
 
-df = pd.DataFrame({'player_identifier': [1,1],
-                   'date_registered': ['2017-12-03','2017-12-03'],
-                   'marketing_channel': ['Facebook', 'Google'],
-                   'day_after_registration': [1,7],
-                   'prata_spent': [182.72,182.72]})
+# df = pd.DataFrame({'player_identifier': [1,1],
+#                    'date_registered': ['2017-12-03','2017-12-03'],
+#                    'marketing_channel': ['Facebook', 'Google'],
+#                    'day_after_registration': [1,7],
+#                    'prata_spent': [182.72,182.72]})
                
 
-print(model_test(df))
+# print(model_test(df))
 
